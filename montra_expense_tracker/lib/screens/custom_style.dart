54ecfaa2e20dce2712b0fda00f9e42b9ccd766db
Tofazzal-2443 <tofazzal15-2443@diu.edu.bd>
2023-05-23 21:56:17 +0600

    custom color all add

--- montra_expense_tracker/lib/screens/custom_style.dart
+++ montra_expense_tracker/lib/screens/custom_style.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
+//Google Font
 
 
+// Text Style
+ myStyle (double size, [Color? clr, FontWeight? fw]){
+  return TextStyle(
+    fontSize: size,
+    color: clr,
+    fontWeight: fw,
+  );
+}
 
 const style36 = TextStyle(
   fontSize: 36,
   fontWeight: FontWeight.w500,
   color: style36Clr,
-
 );
 
 const style32 = TextStyle(
@@ -40,8 +48,13 @@ const buttonTextS2 = TextStyle(
   color: Color(0xff7F3DFF),
 );
 
+const titleX = TextStyle(
+  fontSize: 64,
+
+);
 
 
+// all color code
 const Color backgroundClr=Color(0xff7F3DFF);
 const Color backgroundWClr=Color(0xffE5E5E5);
 const Color buttonClr1=Color(0xff7F3DFF);
@@ -51,3 +64,50 @@ const Color style32Clr=Color(0xff212325);
 const Color style18Clr=Color(0xff212325);
 const Color style16Clr=Color(0xff91919F);
 
+// Dark Color code
+const Color dark25=Color(0xff7A7E80);
+const Color dark50=Color(0xff464A4D);
+const Color dark75=Color(0xff161719);
+const Color dark100=Color(0xff0D0E0F);
+
+// Light Color Code
+const Color light20=Color(0xffE3E5E6);
+const Color light40=Color(0xffF2F4F5);
+const Color light60=Color(0xffF7F9FA);
+const Color light80=Color(0xffFBFBFB);
+const Color light100=Color(0xffFFFFFF);
+
+// Violet Colors Code
+const Color violet20=Color(0xffEEE6FF);
+const Color violet40=Color(0xffD3BDFF);
+const Color violet60=Color(0xffB18AFF);
+const Color violet80=Color(0xff8F57FF);
+const Color violet100=Color(0xff7F3DFF);
+
+//Red Colors Code
+const Color red20=Color(0xffFDD5D7);
+const Color red40=Color(0xffFDA2A9);
+const Color red60=Color(0xffFD6F7A);
+const Color red80=Color(0xffFD5662);
+const Color red100=Color(0xffFD3C4A);
+
+//Green Colors code
+const Color green20=Color(0xffCFFAEA);
+const Color green40=Color(0xff93EACA);
+const Color green60=Color(0xff65D1AA);
+const Color green80=Color(0xff2AB784);
+const Color green100=Color(0xff00A86B);
+
+// Yellow Colors Code
+const Color yellow20=Color(0xffFCEED4);
+const Color yellow40=Color(0xffFCDDA1);
+const Color yellow60=Color(0xffFCCC6F);
+const Color yellow80=Color(0xffFCBB3C);
+const Color yellow100=Color(0xffFCAC12);
+
+//Blue Colors Code
+const Color blue20=Color(0xffBDDCFF);
+const Color blue40=Color(0xff8AC0FF);
+const Color blue60=Color(0xff57A5FF);
+const Color blue80=Color(0xff248AFF);
+const Color blue100=Color(0xff0077FF);
